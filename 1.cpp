552ccb24038d917a4147e0b717f37202417f0a7d
johnneycat <qt19911220@qq.com>
2017-06-15 22:54:25 +0800

    增加了子类调用父类中的函数

--- 1.cpp
+++ 1.cpp
@@ -4,46 +4,49 @@ using namespace std;
 
 class BASE{
 public:
-	BASE(int k)
-	{
-		cout<<"this is base"<<endl;
-		func();
-		i = k;
-		cout<<i<<endl;
-	}
-
-	void func()
-	{
-		cout<<"this is func"<<endl;
-	}
-	int i;
-	int j = 20;
+    BASE(int k)
+    {
+        cout<<"this is base"<<endl;
+        func();
+        i = k;
+        cout<<i<<endl;
+    }
+    
+    void func()
+    {
+        cout<<"this is func"<<endl;
+    }
+    int i;
+    int j = 20;
 };
 
 class CHILD: public BASE
 {
 public:
-	CHILD():BASE(100)
-	{
-		
-		cout<<"this is child"<<endl;
-		
-		
-		func();
-		cout<<i<<endl;
-	}
-
-	void func()
-	{
-		cout<<"this is funcd"<<endl;
-		j = 30;
-	}
-	~CHILD(){};
-	
+    //这样才能正确构造
+    CHILD():BASE(100)
+    {
+        
+        cout<<"this is child"<<endl;
+        
+        
+        func();
+        cout<<i<<endl;
+    }
+    
+    void func()
+    {
+        cout<<"this is funcd"<<endl;
+        j = 30;
+    }
+    ~CHILD(){};
+    
 };
 
 int main()
 {
-	CHILD child;
-	return 0;
+    CHILD child;
+    //需要这样才能调用父类中的方法
+    child.BASE::func();
+    return 0;
 }
